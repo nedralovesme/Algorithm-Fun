0e63328a1e8ff290057091b1e4b06f6e65718f15
Nedra <nedra@Nedras-MacBook-Pro.local>
2016-12-09 11:32:50 -0600

    taking out the checks

--- string.py
+++ string.py
@@ -1,15 +1,14 @@
 ## Write an algorithm that takes in a string and reverses all the characters in that string
-
 #define the string
 my_string = "My spoon is too big"
 
 #split it up
 splitz = my_string.split()
-print splitz
+# print splitz
 
 #flip it
 reverse = splitz[::-1]
-print reverse
+# print reverse
 
 #put it back together
 string_reversed = " ".join(reverse)
